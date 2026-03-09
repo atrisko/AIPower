5d25bd661d1025d0fc091ed8930017b195833fbc
atrisko <alex.trisko@googlemail.com>
2026-03-09 21:23:23 +0100

    Added token counter and verbose flag

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
+
 
 
 
@@ -15,11 +18,33 @@ def main():
     
     client = genai.Client(api_key=api_key)
 
+    parser = argparse.ArgumentParser(description='User prompt for Gemini API')
+    parser.add_argument('prompt', type=str, nargs='+',help='The prompt to send to the Gemini')
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+
+    messages = [types.Content(role="user", parts=[types.Part(text=" ".join(args.prompt))])]
+
     response = client.models.generate_content(
         model='gemini-2.5-flash',
-        contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.')
+        contents=messages)
     
-    print(response.text)
+    if response.usage_metadata is None:
+        raise RuntimeError("Response is missing usage metadata.")
+    
+    if args.verbose:
+
+        print(f"User prompt: {" ".join(args.prompt)}")
+    
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    
+
+        print(response.text)
+
+    else:
+        print(response.text)
 
 
 if __name__ == "__main__":
